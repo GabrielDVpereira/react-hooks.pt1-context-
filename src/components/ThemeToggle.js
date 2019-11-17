0bd7c1aece594a527fe992f52bcded6d1bc90cf5
Gabriel Davi <gabriel.davi.99@gmail.com>
2019-11-17 18:50:30 -0300

    just adding this simple example to git

---
+++ src/components/ThemeToggle.js
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+export function ThemeToggle(){
+    const { toggleTheme } = useContext(ThemeContext);
+
+    return(
+        <button onClick={toggleTheme}>Toggle Theme</button>
+    );
+}
